bf96bba5b0cfc8b49cb9bb309d46dca1de83e6b4
Taha Khan <tahazamir.khan@gmail.com>
2021-10-04 01:27:13 +0500

    first commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = var.location
-    access_key = aws_instance.aws_access_key
-    secret_key = aws_instance.aws_secret_key
+    access_key = aws_access_key
+    secret_key = aws_secret_key
 }
 
 resource "aws_instance" "vm" {
